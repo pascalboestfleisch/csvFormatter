0c5180c76dc1d051041301813401afa6ef8c61e0
Pascal Boestfleisch <pascalboestfleisch@gmail.com>
2024-05-13 21:24:20 +0200

    added setter and getter, also updated readme

--- Booking.java
+++ Booking.java
@@ -1,43 +1,79 @@
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Booking {
-    private double bank_balance = 0;
-    private double fixed_cost = 0;
-    private double joint_income = 0;
 
-    public double getBank_balance() {
+    private double bank_balance = 2133.32;
+    private double fixed_cost = 165.2;
+    private double joint_income = 1633.32;
+    private double childSupport = 500;
+    private double shoppingBudget = 400;
+    private double gezTus = 92;
+    private double car = 147.30;
+    private double rentWlanElec = 875;
 
-        return bank_balance;
+    DecimalFormat df = new DecimalFormat("0.00");
+
+    public String getBank_balance() {
+        return df.format(bank_balance);
     }
 
     public void setBank_balance(double bank_balance) {
-
         this.bank_balance = bank_balance;
     }
 
-    public void bookingDispatch(int dispatch) {
-
-        bank_balance = bank_balance - dispatch;
+    public String getFixed_costs() {
+        return df.format(fixed_cost);
     }
 
-    public void bookingSaldo(int saldo) {
-
-        bank_balance = bank_balance + saldo;
-    }
-
-    public double getFixed_costs() {
-        return fixed_cost;
-    }
-
-    public void setFixed_cost(double fixed_cost) {
+    public void setFixed_costs(double fixed_cost) {
         this.fixed_cost = fixed_cost;
     }
 
-    public double getJoint_income() {
-        return joint_income;
+    public String getJoint_income() {
+        return df.format(joint_income);
     }
 
     public void setJoint_income(double joint_income) {
         this.joint_income = joint_income;
+    }
+
+    public String getChildSupport() {
+        return df.format(childSupport);
+    }
+
+    public void setChildSupport(double childSupport) {
+        this.childSupport = childSupport;
+    }
+
+    public String getShoppingBudget() {
+        return df.format(shoppingBudget);
+    }
+
+    public void setShoppingBudget(double shoppingBudget) {
+        this.shoppingBudget = shoppingBudget;
+    }
+
+    public String getGezTus() {
+        return df.format(gezTus);
+    }
+
+    public void setGezTus(double gezTus) {
+        this.gezTus = gezTus;
+    }
+
+    public String getCar() {
+        return df.format(car);
+    }
+
+    public void setCar(double car) {
+        this.car = car;
+    }
+
+    public String getRentWlanElec() {
+        return df.format(rentWlanElec);
+    }
+
+    public void setRentWlanElec(double rentWlanElec) {
+        this.rentWlanElec = rentWlanElec;
     }
 }
--- out/production/buchhaltung_app/README.md
+++ out/production/buchhaltung_app/README.md
@@ -1,3 +1,13 @@
 Java Projekt zur praktischen Übung.
+---
 
-Private Verwendung, geplant mit Login aus einer DB, ebenso Formatierung der Werte in SQL-Schreibweise zum Import
+**Funktionen:**
+
+  - Werte aus CSV parsen
+  - Ab und Zugänge als Buttons, anschließend Werte als CSV formatiert
+  - CSV-Datei anlegen, bearbeiten und speichern können
+  - Gemeinsames Einkommen in CSV
+  - GUI für Anwendung
+  - Anzeigen von eingefügten Daten in Historie
+    -> eventuell auch schon in Anwendung löschbar
+  - lokale Datei in einem Ordner, Zielort ist ein Ordner im NAS
